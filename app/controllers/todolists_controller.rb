b785fec4972827b5a65b148a8df49e68caaff67d
Kashuu-kaba <Kabashu12@yahoo.ne.jp>
2021-07-30 02:53:15 +0000

    7/30 commi

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,36 +1,37 @@
 class TodolistsController < ApplicationController
-  
-  def new 
+
+  def new
     @list = List.new()
   end
-  
-  def create 
-    @list = List.new(list_params)
-    @list.save
-    redirect_to "/todolists", notice: "作成に成功しました"
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to todolist_path(list), notice: "作成に成功しました"
   end
-  
+
   def index
     @lists = List.all
   end
-  
+
   def show
+    @list = List.find(params[:id])
   end
-    
-  def edit 
+
+  def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   private
   def list_params
     params.require(:list).permit(:title, :body)
   end
-  
+
   def set_params
     @list = List.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  get "todolists/new"
-  get "/top" => "homes#top"
-  post "todolists" => "todolists#create"
-  get "todolists" => "todolists#index"
+  # devise_scope :customers do
+  #   get 'customers/sign_up' => 'devise/registrations#new'
+  # end
+  devise_for :customers, controllers: {
+  sessions: 'users/sessions',
+  registrations: 'users/registrations'
+  }
+
+  # get "todolists/new"
+  # get "/top" => "homes#top"
+  # post "todolists" => "todolists#create"
+  # get "todolists" => "todolists#index"
+  resources :todolists
 end
